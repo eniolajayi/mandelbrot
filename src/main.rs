d3914629cfe1f69331214d248720cb498864bc68
eniolajayi <ajayieniola16@gmail.com>
2022-12-21 14:23:10 +0100

    parse arguments to create mandelbrot png file

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,28 @@ use image::ColorType;
 use num::Complex;
 use std::fs::File;
 use std::str::FromStr;
+use std::env;
 
-fn main() {}
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 5 {
+        eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT", args[0]);
+        eprintln!("Example: {} mandel.png 1000x750 -1.20,0.35 -1.0.20", args[0]);
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x').expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3]).expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
+
+}
 
 /// Parse the string `s` as a coordinate pair, like `"400x600"` or `"1.0,0.5"`.
 ///
