fb4c566971fc391cedc64a588dd0186c27d902b0
eniolajayi <ajayieniola16@gmail.com>
2022-12-21 14:09:22 +0100

    add function to write buffer of pixels to file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use image::png::PNGEncoder;
+use image::ColorType;
 use num::Complex;
+use std::fs::File;
 use std::str::FromStr;
 
 fn main() {}
@@ -151,4 +154,20 @@ fn complex_square_add_loop(c: Complex<f64>) {
     loop {
         z = z * z + c;
     }
+}
+
+/// Write the buffer `pixels`, whose dimensions are given by `bounds`, to the
+/// file named `filename`.
+
+fn write_image(
+    filename: &str,
+    pixels: &[u8],
+    bounds: (usize, usize),
+) -> Result<(), std::io::Error> {
+    let output = File::create(filename)?;
+
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Gray(8))?;
+
+    Ok(())
 }
